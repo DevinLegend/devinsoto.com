546525a8e045526c139434ab181e23f6190e432a
Devin Soto <devinsoto@Devins-MacBook-Air.local>
2025-10-09 18:52:59 -0700

    Add excerpt plugin for post previews

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const excerptPlugin = require("@11ty/eleventy-plugin-excerpt");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(excerptPlugin);
   eleventyConfig.addPassthroughCopy("resources");
   eleventyConfig.addPassthroughCopy("app.js");
 
