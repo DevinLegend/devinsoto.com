b1617630bbe6ddf10277b2bb26a48e32b3bff2ea
Devin Soto <devinsoto@Devins-MacBook-Air.local>
2025-10-09 19:02:33 -0700

    Fix filter syntax and remove invalid plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,4 @@
-const excerptPlugin = require("@11ty/eleventy-plugin-excerpt");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(excerptPlugin);
   eleventyConfig.addPassthroughCopy("resources");
   eleventyConfig.addPassthroughCopy("app.js");
 
